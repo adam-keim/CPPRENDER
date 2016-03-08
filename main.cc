919c7654bd6102f1c92a785b514fe80b5f47f123
Adam Keim <adam.frusa@gmail.com>
2016-03-07 21:00:21 -0600

    Write function to invert matrix
    Also adds a basic test. Use the debugger and it should return
    
    0.707107 -0.331295 0.624695 0 0 0.883452 0.468521 0 -0.707107 -0.331295
    0.624695 0 0 0 -6.404043 1

--- main.cc
+++ main.cc
@@ -83,13 +83,105 @@ public:
     Matrix44 operator*(const Matrix44& rhs) const {
         Matrix44 mult;
         for (uint8_t i = 0; i < 4; i++) {
-            for (uint8_t j = 0; i < 4; i++) {
+            for (uint8_t j = 0; j < 4; j++) {
                 mult[i][j] = m[i][0] * rhs[0][j] +
                         m[i][1] * rhs[1][j] +
                         m[i][2] * rhs[2][j] +
                         m[i][3] * rhs[3][j];
             }
         }
+    }
+
+    Matrix44 transpose() const {
+        Matrix44 transpMap;
+        for (uint8_t i = 0; i < 4; i++) {
+            for (uint8_t j = 0; j < 4; j++) {
+                transpMap[i][j] = m[j][i];
+            }
+        }
+        return transpMap;
+    }
+
+    Matrix44 inverse() {
+        int i, j, k;
+        Matrix44 s;
+        Matrix44 t(*this);
+
+        // Forward elimination
+        for (i = 0; i < 3; i++) {
+            int pivot = i;
+
+            T pivotsize = t[i][i];
+
+            if (pivotsize < 0)
+                pivotsize = -pivotsize;
+
+            for (j = i + 1; j < 4; j++) {
+                T tmp = t[j][i];
+
+                if (tmp < 0)
+                    tmp = -tmp;
+
+                if (tmp > pivotsize) {
+                    pivot = j;
+                    pivotsize = tmp;
+                }
+            }
+
+            if (pivotsize == 0) {
+                // Cannot invert singular matrix
+                return Matrix44();
+            }
+
+            if (pivot != i) {
+                for (j = 0; j < 4; j++) {
+                    T tmp;
+
+                    tmp = t[i][j];
+                    t[i][j] = t[pivot][j];
+                    t[pivot][j] = tmp;
+
+                    tmp = s[i][j];
+                    s[i][j] = s[pivot][j];
+                    s[pivot][j] = tmp;
+                }
+            }
+
+            for (j = i + 1; j < 4; j++) {
+                T f = t[j][i] / t[i][i];
+
+                for (k = 0; k < 4; k++) {
+                    t[j][k] -= f * t[i][k];
+                    s[j][k] -= f * s[i][k];
+                }
+            }
+        }
+
+        // Backward substitution
+        for (i = 3; i >= 0; --i) {
+            T f;
+
+            if ((f = t[i][i]) == 0) {
+                // Cannot invert singular matrix
+                return Matrix44();
+            }
+
+            for (j = 0; j < 4; j++) {
+                t[i][j] /= f;
+                s[i][j] /= f;
+            }
+
+            for (j = 0; j < i; j++) {
+                f = t[j][i];
+
+                for (k = 0; k < 4; k++) {
+                    t[j][k] -= f * t[i][k];
+                    s[j][k] -= f * s[i][k];
+                }
+            }
+        }
+
+        return s;
     }
 
     void multVecMatrix(const Vec3<T> &src, Vec3<T> &dst) const {
@@ -110,14 +202,26 @@ public:
         dst.z = src.x * m[0][2] + src.y * m[1][2] + src.z * m[2][2];
     }
 };
-
+typedef Matrix44<float> Matrix44f; 
 int main() {
-    Vec3<int> v1(8, 5, 1);
-    Vec3<int> v2(4, 5, 6);
-    Matrix44<int> m1;
-    m1[0][0] = 2;
-    m1[1][1] = 2;
-    m1[2][2] = 2;
-    m1.multVecMatrix(v1, v2);
-    cout << m1[0][4];
+    Matrix44f m1;
+    m1[0][0] = 0.707107;
+    m1[0][1] = 0;
+    m1[0][2] = -0.707107;
+    m1[0][3] = 0;
+    m1[1][0] = -0.331295;
+    m1[1][1] = 0.883452;
+    m1[1][2] = -0.331295;
+    m1[1][3] = 0;
+    m1[2][0] = 0.624695;
+    m1[2][1] = 0.468521;
+    m1[2][2] = 0.624695;
+    m1[2][3] = 0;
+    m1[3][0] = 4.000574;
+    m1[3][1] = 3.00043;
+    m1[3][2] = 4.000574;
+    m1[3][3] = 1;
+
+    Matrix44f f2 = m1.inverse();
+    return 0;
 }
