be058fc3875f71f5467e19f9bb38bcedd6e1fc2e
Adam Keim <adam.frusa@gmail.com>
2016-03-07 18:42:52 -0600

    Add basic outline for vector class, add method to find length of vector, still need to import a library that adds the SQRT method
    See title.  I'll work on better commit descriptions in the future

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include <cstdlib> 
+#include <cstdio> 
+#include <iostream> 
+#include <iomanip> 
+ 
+using namespace std;
+
+
+template<typename T>
+class Vec3 {
+    public:
+        Vec3(): x(T(0)), y(T(0)), z(T(0)) {}
+        Vec3(const T &xx): x(xx), y(xx), z(xx) {}
+        Vec3(T xx, T yy, T zz): x(xx), y(yy), z(zz) {}
+        T x, y, z;
+        T length() {
+            return sqrt(x*x+y*y+z*z);
+        }
+};
+int main () {
+}
