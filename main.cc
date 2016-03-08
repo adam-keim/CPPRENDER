d974685f92605902330d32bf19443caf28264bdc
Adam Keim <adam.frusa@gmail.com>
2016-03-07 19:11:05 -0600

    Add matrix multiplication

--- main.cc
+++ main.cc
@@ -65,7 +65,17 @@ class Matrix44 {
         T* operator [] (uint8_t i) { return m[i]; } 
         //Initialize the matrix using coefficients of the identity matrix
         T m[4][4] = {{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
-        
+        Matrix44 operator * (const Matrix44& rhs) const {
+            Matrix44 mult;
+            for (uint8_t i = 0; i <4; i++) {
+                for (uint8_t j = 0; i < 4; i++) {
+                    mult[i][j] = m[i][0] * rhs[0][j] + 
+                         m[i][1] * rhs[1][j] + 
+                         m[i][2] * rhs[2][j] + 
+                         m[i][3] * rhs[3][j];
+                }
+            }
+        }
 };
 int main() {
 }
