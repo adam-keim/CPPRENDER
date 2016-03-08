3638ca1a32cf0923350c35229f57a7b4d5ea207c
Adam Keim <adam.frusa@gmail.com>
2016-03-07 21:32:59 -0600

    Begin writing function to project from 3d into 2d

--- main.cc
+++ main.cc
@@ -3,6 +3,20 @@
 #include <cstdio> 
 #include <fstream> 
 #include <cmath> 
- 
+
 #include "geometry.h" 
- 
+
+bool computePixelCoordinates(
+        const Vec3f &pWorld,
+        const Matrix44f &cameraToWorld,
+        const float &canvasWidth,
+        const float &canvasHeight,
+        const int &imageWidth,
+        const int &imageHeight,
+        Vec2i &pRaster) {
+    Vec3f pCamera;
+    Matrix44f WtC = cameraToWorld.inverse();
+    WtC.multVecMatrix(pWorld, pCamera);
+
+    return true;
+}
