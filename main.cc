29f228ee9649a6a1629be45d2f9bb2f4846a35c3
Adam Keim <adam.frusa@gmail.com>
2016-03-08 21:43:43 -0600

    Format and comment

--- main.cc
+++ main.cc
@@ -5,6 +5,7 @@
 #include <cmath> 
 
 #include "geometry.h" 
+//This function is some of the finest code I have ever written
 
 bool computePixelCoordinates(
         const Vec3f &pWorld,
@@ -20,20 +21,21 @@ bool computePixelCoordinates(
     WtC.multVecMatrix(pWorld, pCamera);
     //Convert to screen space
     Vec2f pScreen;
-    pScreen.x = (pCamera.x/-pCamera.z);
-    pScreen.y = (pCamera.y/-pCamera.z);
+    pScreen.x = (pCamera.x / -pCamera.z);
+    pScreen.y = (pCamera.y / -pCamera.z);
     //Check if on screen
-    if (abs(pScreen.x) >= canvasWidth/2 || abs(pScreen.y >= canvasHeight/2))
+    if (abs(pScreen.x) >= canvasWidth / 2 || abs(pScreen.y >= canvasHeight / 2))
         return false;
     //Normalize
     Vec2f pNdc;
-    pNdc.x = (pScreen.x + canvasWidth/2)/canvasWidth;
-    pNdc.y = (pScreen.y + canvasHeight/2)/canvasHeight;
+    pNdc.x = (pScreen.x + canvasWidth / 2) / canvasWidth;
+    pNdc.y = (pScreen.y + canvasHeight / 2) / canvasHeight;
     //Convert to Raster
     pRaster.x = floor(pNdc.x * imageWidth);
     pRaster.y = floor(pNdc.x * imageHeight);
     return true;
 }
-int main(){
+
+int main() {
     return 0;
 }
