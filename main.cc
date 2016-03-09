9e99fca12db67213ab4a3f18156b5eb59a26121d
Adam Keim <adam.frusa@gmail.com>
2016-03-08 21:39:34 -0600

    Add comments

--- main.cc
+++ main.cc
@@ -15,17 +15,21 @@ bool computePixelCoordinates(
         const int &imageHeight,
         Vec2i &pRaster) {
     Vec3f pCamera;
+    //Compute world to camera matrix
     Matrix44f WtC = cameraToWorld.inverse();
     WtC.multVecMatrix(pWorld, pCamera);
+    //Convert to screen space
     Vec2f pScreen;
     pScreen.x = (pCamera.x/-pCamera.z);
     pScreen.y = (pCamera.y/-pCamera.z);
+    //Check if on screen
     if (abs(pScreen.x) >= canvasWidth/2 || abs(pScreen.y >= canvasHeight/2))
         return false;
+    //Normalize
     Vec2f pNdc;
     pNdc.x = (pScreen.x + canvasWidth/2)/canvasWidth;
     pNdc.y = (pScreen.y + canvasHeight/2)/canvasHeight;
-    
+    //Convert to Raster
     pRaster.x = floor(pNdc.x * imageWidth);
     pRaster.y = floor(pNdc.x * imageHeight);
     return true;
