99ed7f12381488f25c227874b06847cc59952003
Adam Keim <adam.frusa@gmail.com>
2016-03-08 21:24:44 -0600

    Compact if, add transform to NDC

--- main.cc
+++ main.cc
@@ -20,9 +20,11 @@ bool computePixelCoordinates(
     Vec2f pScreen;
     pScreen.x = (pCamera.x/-pCamera.z);
     pScreen.y = (pCamera.y/-pCamera.z);
-    if (abs(pScreen.x) >= canvasWidth/2 || abs(pScreen.y >= canvasHeight/2)) {
+    if (abs(pScreen.x) >= canvasWidth/2 || abs(pScreen.y >= canvasHeight/2))
         return false;
-    }
+    Vec2f pNdc;
+    pNdc.x = (pScreen.x + canvasWidth/2)/canvasWidth;
+    pNdc.y = (pScreen.y + canvasHeight/2)/canvasHeight;
     return true;
 }
 int main(){
