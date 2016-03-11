023ea9aeec7df7e862656626a7b74cddb888c55d
Adam Keim <adam.frusa@gmail.com>
2016-03-10 21:06:46 -0600

    Finish refactoring

--- pcoords.h
+++ pcoords.h
@@ -3,7 +3,7 @@
 using namespace std;
 bool computePixelCoordinates(
         const Vec3f &pWorld,
-        const Matrix44f &WtC,
+        const Matrix44f &worldToCamera,
         const float &l,
         const float &r,
         const float &t,
@@ -14,7 +14,7 @@ bool computePixelCoordinates(
         Vec2i &pRaster) {
     Vec3f pCamera;
     //Compute world to camera matrix
-    WtC.multVecMatrix(pWorld, pCamera);
+    worldToCamera.multVecMatrix(pWorld, pCamera);
     //Convert to screen space
     Vec2f pScreen;
     pScreen.x = pCamera.x / -pCamera.z * near;
@@ -22,11 +22,11 @@ bool computePixelCoordinates(
     //Check if on screen
 
     //Normalize
-    Vec2f pNDC;
-    pNDC.x = (pScreen.x + r) / (2 * r);
-    pNDC.y = (pScreen.y + t) / (2 * t);
-    pRaster.x = (pNDC.x * imageWidth);
-    pRaster.y = ((1 - pNDC.y) * imageHeight);
+    Vec2f pNdc;
+    pNdc.x = (pScreen.x + r) / (2 * r);
+    pNdc.y = (pScreen.y + t) / (2 * t);
+    pRaster.x = (pNdc.x * imageWidth);
+    pRaster.y = ((1 - pNdc.y) * imageHeight);
     //Convert to Raster
     bool visible = true;
     if (pScreen.x < l || pScreen.x > r || pScreen.y < b || pScreen.y > t)
